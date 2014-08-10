6d9b67ae67b3bad90bc07f32b7822f062ff01eb6
Brandon Beck <brandon.beck@bazaarvoice.com>
2014-08-10 14:49:38 -0500

    Update copyright dates

--- src/test/java/org/isomorphism/util/TokenBucketImplTest.java
+++ src/test/java/org/isomorphism/util/TokenBucketImplTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Brandon Beck
+ * Copyright 2012-2014 Brandon Beck
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
