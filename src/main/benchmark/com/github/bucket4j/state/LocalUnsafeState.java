7df823099a5345c628b4ace2ab2d294041ed1aaf
vladimir.bukhtoyarov <jsecoder@mail.ru>
2017-04-02 22:48:44 +0300

    #22  base-line benchmarks for non-contended cases

---
+++ src/main/benchmark/com/github/bucket4j/state/LocalUnsafeState.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *   Copyright 2015-2017 Vladimir Bukhtoyarov
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.github.bucket4j.state;
+
+import com.github.bucket4j.Bandwidth;
+import com.github.bucket4j.Bucket;
+import com.github.bucket4j.Bucket4j;
+import com.github.bucket4j.local.SynchronizationStrategy;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.time.Duration;
+
+@State(Scope.Benchmark)
+public class LocalUnsafeState {
+
+    public final Bucket bucket = Bucket4j.builder()
+            .withMillisecondPrecision()
+            .addLimit(
+                    Bandwidth.simple(Long.MAX_VALUE / 2, Duration.ofNanos(Long.MAX_VALUE / 2))
+            ).build(SynchronizationStrategy.NONE);
+
+}
