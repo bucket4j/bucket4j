a663a89ba43c7d9ff7d1e6d39c89c9396d57ffcd
vladimir.bukhtoyarov <jsecoder@mail.ru>
2017-05-12 09:33:50 +0300

    #34 integration with sonar-cube

---
+++ sonarcube.sh
@@ -0,0 +1,3 @@
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar \
+-Dsonar.host.url=https://sonarqube.com -Dsonar.login=$my.sonar.login \
+-Dsonar.organization=vladimir-bukhtoyarov-github -e -X
