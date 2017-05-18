6684d862979670c632f6b7f8cda28dde60007719
vladimir.bukhtoyarov <vladimir.bukhtoyarov@nordigy.ru>
2017-05-18 19:14:47 +0300

    #34 finalize integration with sonarcube

--- sonarcube.sh
+++ sonarcube.sh
@@ -1,5 +1,6 @@
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar \
--Dsonar.host.url=https://sonarqube.com -Dsonar.login=$my.sonar.login \
--Dsonar.organization=vladimir-bukhtoyarov-github -e -X \
--Dsonar.dynamicAnalysis=reuseReports
--P sonar
+mvn clean install -P coverage
+
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar \
+-Dsonar.host.url=https://sonarqube.com \
+-Dsonar.login=$MY_SONAR_LOGIN \
+-Dsonar.organization=vladimir-bukhtoyarov-github
