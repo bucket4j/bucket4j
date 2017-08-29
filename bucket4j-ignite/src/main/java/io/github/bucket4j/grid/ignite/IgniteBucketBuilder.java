4dd2b229f7f32d7b263527a70ba885ab3a38e46f
vladimir.bukhtoyarov <vladimir.bukhtoyarov@nordigy.ru>
2017-08-29 12:22:02 +0300

    #41 initial support for ignite

---
+++ bucket4j-ignite/src/main/java/io/github/bucket4j/grid/ignite/IgniteBucketBuilder.java
@@ -0,0 +1,73 @@
+/*
+ *  Copyright 2015-2017 Vladimir Bukhtoyarov
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package io.github.bucket4j.grid.ignite;
+
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.ConfigurationBuilder;
+import io.github.bucket4j.grid.GridBucket;
+import io.github.bucket4j.grid.GridBucketState;
+import io.github.bucket4j.grid.RecoveryStrategy;
+
+import javax.cache.Cache;
+import java.io.Serializable;
+
+/**
+ * {@inheritDoc}
+ *
+ * This builder creates the buckets backed by any <a href="https://www.jcp.org/en/jsr/detail?id=107">JCache API (JSR 107)</a> implementation.
+ *
+ */
+public class IgniteBucketBuilder extends ConfigurationBuilder<IgniteBucketBuilder> {
+
+    /**
+     * Creates the new instance of {@link IgniteBucketBuilder}
+     */
+    public IgniteBucketBuilder() {
+        super();
+    }
+
+    /**
+     * Constructs an instance of {@link GridBucket} which state actually stored inside in-memory data-grid,
+     * the bucket stored in the grid immediately, so one network request will be issued to grid.
+     * Due to this method performs network IO, returned result must not be treated as light-weight entity,
+     * it will be a performance anti-pattern to use this method multiple times for same key,
+     * you need to cache result somewhere and reuse between invocations,
+     * else performance of all operation with bucket will be 2-x times slower.
+     *
+     * <p>
+     * Use this method if and only if you need to full control over bucket lifecycle(especially specify {@link RecoveryStrategy}),
+     * and you have clean caching strategy which suitable for storing buckets,
+     * else it would be better to work through {@link JCache#proxyManagerForCache(Cache) ProxyManager},
+     * which does not require any caching, because ProxyManager operates with light-weight versions of buckets.
+     *
+     * @param cache distributed cache which will hold bucket inside cluster.
+     *             Feel free to store inside single {@code cache} as mush buckets as you need.
+     * @param key  for storing bucket inside {@code cache}.
+     *             If you plan to store multiple buckets inside single {@code cache}, then each bucket should has own unique {@code key}.
+     * @param recoveryStrategy specifies the reaction which should be applied in case of previously saved state of bucket has been lost.
+     *
+     * @return new distributed bucket
+     */
+    public <K extends Serializable> Bucket build(Cache<K, GridBucketState> cache, K key, RecoveryStrategy recoveryStrategy) {
+        BucketConfiguration configuration = buildConfiguration();
+        JCacheProxy<K> gridProxy = new JCacheProxy<>(cache);
+        return GridBucket.createInitializedBucket(key, configuration, gridProxy, recoveryStrategy);
+    }
+
+}
