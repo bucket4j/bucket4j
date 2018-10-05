6675d51721ed908c8d976a4e19c7ba34409753e7
vladimir.bukhtoyarov <vladimir.bukhtoyarov@nordigy.ru>
2018-10-05 12:29:02 +0300

    #36

---
+++ bucket4j-redis/src/main/resources/lua/redis/entry_processor.lua
@@ -0,0 +1,49 @@
+-- This is direct analog of io.github.bucket4j.grid.jcache.JCacheEntryProcessor
+
+local bucket4jExecute = function (key, currentTimeNanos, command)
+-- TODO
+end
+
+local bucket4jInitState = function (key, currentTimeNanos, configuration)
+    -- TODO
+end
+
+local bucket4jInitStateAndExecute = function (key, currentTimeNanos, configuration, command)
+    -- TODO
+end
+
+-- Allow to use time related functions
+redis.replicate_commands();
+
+local commandType = ARGV[1];
+local currentTimeNanos = ARGV[2];
+if currentTimeNanos == "unset" then
+    local time = TIME();
+    currentTimeNanos = time[1] * 1000000000 + time[2] * 1000;
+else
+    currentTimeNanos = tonumber(currentTimeNanos);
+end
+
+if commandType == "execute" then
+    local commandJson = ARGV[3];
+    local command = cjson.decode(commandJson);
+    return bucket4jExecute(key, currentTimeNanos, command);
+end
+
+if commandType == "initState" then
+    local configurationJson = ARGV[3];
+    local configuration = cjson.decode(configurationJson);
+    return bucket4jInitState(key, currentTimeNanos, configuration);
+end
+
+if commandType == "execute" then
+    local configurationJson = ARGV[3];
+    local configuration = cjson.decode(configurationJson);
+    local commandJson = ARGV[3];
+    local command = cjson.decode(commandJson);
+    return bucket4jInitStateAndExecute(key, currentTimeNanos, configuration, command);
+end
+
+return redis.error_reply("Unknown command type [" .. commandType .. "]")
+
+
--- bucket4j-redis/src/main/resources/lua/redis/execute.lua
+++
@@ -1,5 +0,0 @@
--- Allow to use time related functions
-redis.replicate_commands();
-
-local key = KEYS[1];
-local commandJson = ARGV[1];
