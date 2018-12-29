41580fc36b9f185988cd17bdba909551f6ce40b5
vladimir.bukhtoyarov <vladimir.bukhtoyarov@nordigy.ru>
2018-12-29 16:16:01 +0300

    increasing stability of integration tests for Apache Ignite via replacing multicast discovery by preconfigured ip address which always points to localhost

--- bucket4j-ignite/src/test/java/io/github/bucket4j/grid/ignite/IgniteTest.java
+++ bucket4j-ignite/src/test/java/io/github/bucket4j/grid/ignite/IgniteTest.java
@@ -28,6 +28,8 @@ import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.gridkit.nanocloud.Cloud;
 import org.gridkit.nanocloud.CloudFactory;
 import org.gridkit.nanocloud.VX;
@@ -37,6 +39,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
 
 public class IgniteTest extends AbstractDistributedBucketTest<IgniteBucketBuilder, io.github.bucket4j.grid.ignite.Ignite> {
 
@@ -47,22 +52,40 @@ public class IgniteTest extends AbstractDistributedBucketTest<IgniteBucketBuilde
     private static Ignite ignite;
 
     @BeforeClass
-    public static void setup() {
+    public static void setup() throws UnknownHostException {
         // start separated JVM on current host
         cloud = CloudFactory.createCloud();
         cloud.node("**").x(VX.TYPE).setLocal();
         server = cloud.node("stateful-ignite-server");
 
+        int serverDiscoveryPort = 47500;
+        String serverNodeAdress = InetAddress.getLocalHost().getHostAddress() + ":" + serverDiscoveryPort;
+
         server.exec((Runnable & Serializable) () -> {
+            TcpDiscoveryVmIpFinder neverFindOthers = new TcpDiscoveryVmIpFinder();
+            neverFindOthers.setAddresses(Collections.singleton(serverNodeAdress));
+
+            TcpDiscoverySpi tcpDiscoverySpi = new TcpDiscoverySpi();
+            tcpDiscoverySpi.setIpFinder(neverFindOthers);
+            tcpDiscoverySpi.setLocalPort(serverDiscoveryPort);
+
             IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
             igniteConfiguration.setClientMode(false);
+            igniteConfiguration.setDiscoverySpi(tcpDiscoverySpi);
+
             CacheConfiguration cacheConfiguration = new CacheConfiguration("my_buckets");
             Ignite ignite = Ignition.start(igniteConfiguration);
             ignite.getOrCreateCache(cacheConfiguration);
         });
 
         // start ignite client which works inside current JVM and does not hold data
+        TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
+        ipFinder.setAddresses(Collections.singleton(serverNodeAdress));
+        TcpDiscoverySpi tcpDiscoverySpi = new TcpDiscoverySpi();
+        tcpDiscoverySpi.setIpFinder(ipFinder);
+
         IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
+        igniteConfiguration.setDiscoverySpi(tcpDiscoverySpi);
         igniteConfiguration.setClientMode(true);
         ignite = Ignition.start(igniteConfiguration);
         CacheConfiguration cacheConfiguration = new CacheConfiguration("my_buckets");
