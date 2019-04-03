146c9510b9a045114bf056898d344ab85db0ebec
vladimir.bukhtoyarov <vladimir.bukhtoyarov@nordigy.ru>
2019-04-03 12:57:38 +0300

    #72 unit tests for Oracle Coherence integration

--- bucket4j-coherence/src/test/java/CoherenceTest.java
+++ bucket4j-coherence/src/test/java/CoherenceTest.java
@@ -18,6 +18,13 @@
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.NamedCache;
+import io.github.bucket4j.AbstractDistributedBucketTest;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.grid.ProxyManager;
+import io.github.bucket4j.grid.RecoveryStrategy;
+import io.github.bucket4j.grid.coherence.Coherence;
+import io.github.bucket4j.grid.coherence.CoherenceBucketBuilder;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -26,9 +33,7 @@ import org.littlegrid.ClusterMemberGroupUtils;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class CoherenceInfrastructureTest {
-
-    private static final String KEY = "42";
+public class CoherenceTest  extends AbstractDistributedBucketTest<CoherenceBucketBuilder, Coherence> {
 
     private static ClusterMemberGroup memberGroup;
     private static NamedCache cache;
@@ -50,6 +55,32 @@ public class CoherenceInfrastructureTest {
     public void testThatCoherenceCanBeStarted() {
         cache.put("13", "42");
         assertEquals("42", cache.get("13"));
+    }
+
+    @Override
+    protected Class<Coherence> getExtensionClass() {
+        return Coherence.class;
+    }
+
+    @Override
+    protected Bucket build(CoherenceBucketBuilder builder, String key, RecoveryStrategy recoveryStrategy) {
+        return builder.build(cache, key, recoveryStrategy);
+    }
+
+    @Override
+    protected ProxyManager<String> newProxyManager() {
+        return Bucket4j.extension(getExtensionClass()).proxyManagerForCache(cache);
+    }
+
+    @Override
+    protected void removeBucketFromBackingStorage(String key) {
+        cache.remove(key);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Override
+    public void testThatImpossibleToPassNullCacheToProxyManagerConstructor() {
+        Bucket4j.extension(getExtensionClass()).proxyManagerForCache(null);
     }
 
 }
