509d55c4923ec0750898014017f868c720e3df78
vladimir.bukhtoyarov <jsecoder@mail.ru>
2019-10-27 22:03:34 +0300

    #90 add example which shows 150000 request/second throughput on single Hazelcast bucket with 100 concurrent threads

---
+++ bucket4j-examples/src/test/java/example/distributed/optimizers/batching/HazelcastWithBatchingPerformanceExample.java
@@ -0,0 +1,137 @@
+package example.distributed.optimizers.batching;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Snapshot;
+import com.github.rollingmetrics.dropwizard.adapter.DropwizardAdapters;
+import com.github.rollingmetrics.histogram.OverflowResolver;
+import com.github.rollingmetrics.histogram.hdr.RollingHdrHistogram;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.distributed.proxy.optimizers.batch.BatchingOptimizer;
+import io.github.bucket4j.distributed.remote.RemoteBucketState;
+import org.gridkit.nanocloud.Cloud;
+import org.gridkit.nanocloud.CloudFactory;
+import org.gridkit.nanocloud.VX;
+import org.gridkit.vicluster.ViNode;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import io.github.bucket4j.grid.hazelcast.HazelcastBackend;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class HazelcastWithBatchingPerformanceExample {
+
+    private static Logger logger = LoggerFactory.getLogger(HazelcastWithBatchingPerformanceExample.class);
+
+    private static IMap<String, RemoteBucketState> map;
+    private static Cloud cloud;
+    private static ViNode server;
+
+    private static HazelcastInstance hazelcastInstance;
+
+    @BeforeClass
+    public static void setup() {
+        // start separated JVM on current host
+        cloud = CloudFactory.createCloud();
+        cloud.node("**").x(VX.TYPE).setLocal();
+        server = cloud.node("stateful-hazelcast-server");
+
+        server.exec((Runnable & Serializable) () -> {
+            Config config = new Config();
+            config.setLiteMember(false);
+            HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+            hazelcastInstance.getMap("my_buckets");
+        });
+
+        // start hazelcast client which works inside current JVM and does not hold data
+        Config config = new Config();
+        config.setLiteMember(true);
+        hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+        map = hazelcastInstance.getMap("my_buckets");
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        if (hazelcastInstance != null) {
+            hazelcastInstance.shutdown();
+        }
+        if (cloud != null) {
+            cloud.shutdown();
+        }
+    }
+
+
+    @Test
+    public void benchmarkSyncBucket() throws InterruptedException {
+        Meter consumptionRate = new Meter();
+        com.codahale.metrics.Timer latencyTimer = buildLatencyTimer();
+
+        HazelcastBackend<String> backend = new HazelcastBackend<>(map);
+        BucketConfiguration configuration = BucketConfiguration.builder()
+                .addLimit(
+                    Bandwidth.simple(10, Duration.ofSeconds(1)).withInitialTokens(0))
+                .build();
+
+        Bucket bucket = backend.builder()
+                .withRequestOptimizer(new BatchingOptimizer())
+                .buildProxy("13", configuration);
+
+        Timer statLogTimer = new Timer();
+        statLogTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Consumption rate " + consumptionRate.getOneMinuteRate() + " tokens/sec");
+                System.out.println("Operations with bucket rate " + latencyTimer.getOneMinuteRate() + " ops/sec");
+                Snapshot snapshot = latencyTimer.getSnapshot();
+                System.out.println("Operations with bucket latency:" +
+                        " mean=" + TimeUnit.NANOSECONDS.toMicros((long) snapshot.getMean()) + "micros" +
+                        " median=" + TimeUnit.NANOSECONDS.toMicros((long)snapshot.getMedian()) + "micros" +
+                        " max=" + TimeUnit.NANOSECONDS.toMillis(snapshot.getMax()) + "millis");
+                System.out.println("---------------------------------------------");
+            }
+        }, 1_000, 1_000);
+
+        int PARALLEL_THREADS = 100;
+        // start
+        for (int i = 0; i < PARALLEL_THREADS; i++) {
+            Thread thread = new Thread(() -> {
+                while (true) {
+                    try (com.codahale.metrics.Timer.Context ctx = latencyTimer.time()) {
+                        if (bucket.tryConsume(1)) {
+                            consumptionRate.mark();
+                        }
+                    } catch (Throwable t) {
+                        logger.error("Failed to consume tokens from bucket", t);
+                    }
+                }
+            });
+            thread.setName("Bucket consumer " + i);
+            thread.start();
+        }
+
+        Thread.currentThread().join();
+    }
+
+    private com.codahale.metrics.Timer buildLatencyTimer() {
+        RollingHdrHistogram histogram = RollingHdrHistogram.builder()
+                .withSignificantDigits(2)
+                .resetReservoirPeriodicallyByChunks(Duration.ofSeconds(60), 3)
+                .withHighestTrackableValue(1_000_000_000_000L, OverflowResolver.REDUCE_TO_HIGHEST_TRACKABLE)
+                .build();
+        return DropwizardAdapters.convertToTimer(histogram);
+    }
+
+}
