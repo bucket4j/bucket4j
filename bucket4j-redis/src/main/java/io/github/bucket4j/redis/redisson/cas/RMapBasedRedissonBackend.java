1a91b002fa90bb0a41fa2723a86c0c75d0314167
vladimir.bukhtoyarov <jsecoder@mail.ru>
2021-04-16 00:35:19 +0300

    #161 #36 change status of Redis support from experimental to stable

---
+++ bucket4j-redis/src/main/java/io/github/bucket4j/redis/redisson/cas/RMapBasedRedissonBackend.java
@@ -0,0 +1,100 @@
+/*
+ *
+ * Copyright 2015-2019 Vladimir Bukhtoyarov
+ *
+ *       Licensed under the Apache License, Version 2.0 (the "License");
+ *       you may not use this file except in compliance with the License.
+ *       You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+
+package io.github.bucket4j.redis.redisson.cas;
+
+import io.github.bucket4j.distributed.proxy.ClientSideConfig;
+import io.github.bucket4j.distributed.proxy.generic.compare_and_swap.AbstractCompareAndSwapBasedBackend;
+import io.github.bucket4j.distributed.proxy.generic.compare_and_swap.AsyncCompareAndSwapOperation;
+import io.github.bucket4j.distributed.proxy.generic.compare_and_swap.CompareAndSwapOperation;
+import org.redisson.api.RFuture;
+import org.redisson.api.RMap;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public class RMapBasedRedissonBackend<K> extends AbstractCompareAndSwapBasedBackend<K> {
+
+    private final RMap<K, byte[]> buckets;
+
+    public RMapBasedRedissonBackend(RMap<K, byte[]> buckets, ClientSideConfig clientSideConfig) {
+        super(clientSideConfig);
+        this.buckets = buckets;
+    }
+
+    @Override
+    protected CompareAndSwapOperation beginCompareAndSwapOperation(K key) {
+        return new CompareAndSwapOperation() {
+            @Override
+            public Optional<byte[]> getStateData() {
+                byte[] persistedState = buckets.get(key);
+                return Optional.ofNullable(persistedState);
+            }
+            @Override
+            public boolean compareAndSwap(byte[] originalData, byte[] newData) {
+                if (originalData == null) {
+                    // Redisson prohibits the usage null as values, so "replace" must not be used in such cases
+                    return buckets.putIfAbsent(key, newData) == null;
+                } else {
+                    return buckets.replace(key, originalData, newData);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected AsyncCompareAndSwapOperation beginAsyncCompareAndSwapOperation(K key) {
+        return new AsyncCompareAndSwapOperation() {
+            @Override
+            public CompletableFuture<Optional<byte[]>> getStateData() {
+                RFuture<byte[]> redissonFuture = buckets.getAsync(key);
+                return convertFuture(redissonFuture)
+                    .thenApply((byte[] resultBytes) -> Optional.ofNullable(resultBytes));
+            }
+            @Override
+            public CompletableFuture<Boolean> compareAndSwap(byte[] originalData, byte[] newData) {
+                if (originalData == null) {
+                    // Redisson prohibits the usage null as values, so "replace" must not be used in such cases
+                    RFuture<byte[]> redissonFuture = buckets.putIfAbsentAsync(key, newData);
+                    return convertFuture(redissonFuture).thenApply(Objects::isNull);
+                } else {
+                    RFuture<Boolean> redissonFuture = buckets.replaceAsync(key, originalData, newData);
+                    return convertFuture(redissonFuture);
+                }
+            }
+        };
+    }
+
+    @Override
+    public boolean isAsyncModeSupported() {
+        return true;
+    }
+
+    private <T> CompletableFuture<T> convertFuture(RFuture<T> redissonFuture) {
+        CompletableFuture<T> jdkFuture = new CompletableFuture<>();
+        redissonFuture.whenComplete((result, error) -> {
+           if (error != null) {
+               jdkFuture.completeExceptionally(error);
+           } else {
+               jdkFuture.complete(result);
+           }
+        });
+        return jdkFuture;
+    }
+
+}
---
+++ bucket4j-redis/src/test/java/io/github/bucket4j/redis/redisson/cas/RMapBasedRedissonBackendTest.java
@@ -0,0 +1,67 @@
+package io.github.bucket4j.redis.redisson.cas;
+
+import io.github.bucket4j.distributed.proxy.Backend;
+import io.github.bucket4j.distributed.proxy.ClientSideConfig;
+import io.github.bucket4j.tck.AbstractDistributedBucketTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.redisson.Redisson;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.testcontainers.containers.GenericContainer;
+
+public class RMapBasedRedissonBackendTest extends AbstractDistributedBucketTest {
+
+    private static String MAP_NAME = "buckets";
+
+    private static GenericContainer container;
+    private static RedissonClient redisson;
+    private static RMap<String, byte[]> buckets;
+
+    @BeforeClass
+    public static void setup() {
+        container = startRedisContainer();
+        redisson = createRedissonClient(container);
+        buckets = redisson.getMap(MAP_NAME);
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        if (redisson != null) {
+            redisson.shutdown();
+        }
+        if (container != null) {
+            container.close();
+        }
+    }
+
+    private static RedissonClient createRedissonClient(GenericContainer container) {
+        String redisAddress = container.getContainerIpAddress();
+        Integer redisPort = container.getMappedPort(6379);
+        String redisUrl = "redis://" + redisAddress + ":" + redisPort;
+
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisUrl);
+
+        return Redisson.create(config);
+    }
+
+    private static GenericContainer startRedisContainer() {
+        GenericContainer genericContainer = new GenericContainer("redis:4.0.11")
+                .withExposedPorts(6379);
+        genericContainer.start();
+        return genericContainer;
+    }
+
+    @Override
+    protected Backend<String> getBackend() {
+        return new RMapBasedRedissonBackend<>(buckets, ClientSideConfig.getDefault());
+    }
+
+    @Override
+    protected void removeBucketFromBackingStorage(String key) {
+        buckets.remove(key);
+    }
+
+}
