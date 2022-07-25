9481bfa99d952f981d57b8e562c13bb43004f749
vladimir.bukhtoyarov <jsecoder@mail.ru>
2022-07-25 18:48:42 +0300

    #279 Proofs that there is no bug in Bucket4j itself

--- bucket4j-redis/src/test/java/io/github/bucket4j/redis/redisson/cas/RedissonBasedProxyManagerTest.java
+++ bucket4j-redis/src/test/java/io/github/bucket4j/redis/redisson/cas/RedissonBasedProxyManagerTest.java
@@ -1,11 +1,13 @@
 package io.github.bucket4j.redis.redisson.cas;
 
+import io.github.bucket4j.*;
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.distributed.proxy.ClientSideConfig;
 import io.github.bucket4j.tck.AbstractDistributedBucketTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.redisson.command.CommandExecutor;
 import org.redisson.command.CommandSyncService;
 import org.redisson.config.Config;
@@ -13,7 +15,11 @@ import org.redisson.config.ConfigSupport;
 import org.redisson.connection.ConnectionManager;
 import org.testcontainers.containers.GenericContainer;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class RedissonBasedProxyManagerTest extends AbstractDistributedBucketTest<String> {
 
@@ -35,6 +41,33 @@ public class RedissonBasedProxyManagerTest extends AbstractDistributedBucketTest
         }
         if (container != null) {
             container.close();
+        }
+    }
+
+    @Test
+    public void test_Issue_279() {
+        RedissonBasedProxyManager proxyManager = RedissonBasedProxyManager.builderFor(commandExecutor)
+                .withExpirationStrategy(ExpirationAfterWriteStrategy.none())
+                .build();
+
+        int MIN_CAPACITY = 4;
+        int MAX_CAPACITY = 10;
+        BucketConfiguration configuration = BucketConfiguration.builder()
+                .addLimit(Bandwidth.classic(MIN_CAPACITY, Refill.intervally(4, Duration.ofMinutes(20))))
+                .addLimit(Bandwidth.classic(MAX_CAPACITY, Refill.intervally(10, Duration.ofMinutes(60))))
+                .build();
+
+        Bucket bucket = proxyManager.builder().build("something", configuration);
+
+        for (int i = 1; i <= 4; i++) {
+            VerboseResult<ConsumptionProbe> verboseResult = bucket.asVerbose().tryConsumeAndReturnRemaining(1);
+            ConsumptionProbe probe = verboseResult.getValue();
+            long[] availableTokensPerEachBandwidth = verboseResult.getDiagnostics().getAvailableTokensPerEachBandwidth();
+            System.out.println("Remaining tokens = " + probe.getRemainingTokens());
+            System.out.println("Tokens per bandwidth = " + Arrays.toString(availableTokensPerEachBandwidth));
+            assertEquals(MIN_CAPACITY - i, probe.getRemainingTokens());
+            assertEquals(MIN_CAPACITY - i, availableTokensPerEachBandwidth[0]);
+            assertEquals(MAX_CAPACITY - i, availableTokensPerEachBandwidth[1]);
         }
     }
 
--- bucket4j-redis/src/test/java/io/github/bucket4j/redis/spring/cas/SpringBasedProxyManagerTest.java
+++ bucket4j-redis/src/test/java/io/github/bucket4j/redis/spring/cas/SpringBasedProxyManagerTest.java
@@ -1,18 +1,24 @@
 package io.github.bucket4j.redis.spring.cas;
 
+import io.github.bucket4j.*;
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.redisson.cas.RedissonBasedProxyManager;
 import io.github.bucket4j.tck.AbstractDistributedBucketTest;
 import io.lettuce.core.RedisClient;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.springframework.data.redis.connection.RedisCommands;
 import org.springframework.data.redis.connection.lettuce.LettuceConnection;
 import org.testcontainers.containers.GenericContainer;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class SpringBasedProxyManagerTest extends AbstractDistributedBucketTest<byte[]> {
 
@@ -32,6 +38,34 @@ public class SpringBasedProxyManagerTest extends AbstractDistributedBucketTest<b
         }
         if (container != null) {
             container.close();
+        }
+    }
+
+    @Test
+    public void test_Issue_279() {
+        SpringDataRedisBasedProxyManager proxyManager = SpringDataRedisBasedProxyManager.builderFor(redisCommands)
+                .withExpirationStrategy(ExpirationAfterWriteStrategy.none())
+                .build();
+
+        int MIN_CAPACITY = 4;
+        int MAX_CAPACITY = 10;
+        BucketConfiguration configuration = BucketConfiguration.builder()
+                .addLimit(Bandwidth.classic(MIN_CAPACITY, Refill.intervally(4, Duration.ofMinutes(20))))
+                .addLimit(Bandwidth.classic(MAX_CAPACITY, Refill.intervally(10, Duration.ofMinutes(60))))
+                .build();
+
+        byte[] key = "something".getBytes(StandardCharsets.UTF_8);
+        Bucket bucket = proxyManager.builder().build(key, configuration);
+
+        for (int i = 1; i <= 4; i++) {
+            VerboseResult<ConsumptionProbe> verboseResult = bucket.asVerbose().tryConsumeAndReturnRemaining(1);
+            ConsumptionProbe probe = verboseResult.getValue();
+            long[] availableTokensPerEachBandwidth = verboseResult.getDiagnostics().getAvailableTokensPerEachBandwidth();
+            System.out.println("Remaining tokens = " + probe.getRemainingTokens());
+            System.out.println("Tokens per bandwidth = " + Arrays.toString(availableTokensPerEachBandwidth));
+            assertEquals(MIN_CAPACITY - i, probe.getRemainingTokens());
+            assertEquals(MIN_CAPACITY - i, availableTokensPerEachBandwidth[0]);
+            assertEquals(MAX_CAPACITY - i, availableTokensPerEachBandwidth[1]);
         }
     }
 
