14dbfe7b192e41d2b4eb7194970038c7fb24ed22
Vladimir Buhtoyarov <v.bukhtoyarov@vk.team>
2023-11-02 21:32:34 +0300

    #419 trying to reproduce

--- bucket4j-redis/src/test/java/io/github/bucket4j/redis/jedis/cas/JedisBasedProxyManagerTest.java
+++ bucket4j-redis/src/test/java/io/github/bucket4j/redis/jedis/cas/JedisBasedProxyManagerTest.java
@@ -1,14 +1,20 @@
 package io.github.bucket4j.redis.jedis.cas;
 
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.TokensInheritanceStrategy;
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.tck.AbstractDistributedBucketTest;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import redis.clients.jedis.JedisPool;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.UUID;
 
 public class JedisBasedProxyManagerTest extends AbstractDistributedBucketTest<byte[]> {
@@ -56,4 +62,27 @@ public class JedisBasedProxyManagerTest extends AbstractDistributedBucketTest<by
     protected byte[] generateRandomKey() {
         return UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
     }
+
+    @Test
+    public void testConfigurationReplacement() throws InterruptedException {
+        ProxyManager<byte[]> jedisBasedProxyManager = getProxyManager();
+        for (TokensInheritanceStrategy strategy : TokensInheritanceStrategy.values()) {
+            System.out.println("=========================");
+            System.out.println("Setting bandwith to 5/min");
+            // Bucket bucket = Bucket.builder().addLimit(limit -> limit.capacity(5).refillGreedy(5, Duration.ofMinutes(1))).build();
+            Bucket bucket = jedisBasedProxyManager.builder().build(strategy.name().getBytes(), () -> BucketConfiguration.builder()
+                .addLimit(limit -> limit.capacity(5).refillGreedy(5, Duration.ofMinutes(1))).build());
+            Assertions.assertTrue(bucket.tryConsume(5));
+            Assertions.assertFalse(bucket.tryConsume(1));
+            System.out.println("Update configuration to 10/min using strategy " + strategy);
+            BucketConfiguration newConfig = BucketConfiguration.builder()
+                .addLimit(limit -> limit.capacity(10).refillGreedy(10, Duration.ofMinutes(1))).build();
+            bucket.replaceConfiguration(newConfig, strategy);
+            System.out.println("Sleeping 60+ seconds in order to refill all tokens");
+            Thread.sleep(61000);
+            Assertions.assertTrue(bucket.tryConsume(10));
+            Assertions.assertFalse(bucket.tryConsume(1));
+        }
+    }
+
 }
