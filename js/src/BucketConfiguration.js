5e3be332b2cd618f3fa3c3af1ff1ed4eae0cd2ba
Vladimir Buhtoyarov <v.bukhtoyarov@vk.team>
2023-12-20 14:46:37 +0300

    #410 setup of Javascript build infrastructure based on Webpack

---
+++ js/src/BucketConfiguration.js
@@ -0,0 +1,5 @@
+export class BucketConfiguration {
+    constructor() {
+        // TODO
+    }
+}
---
+++ js/src/bucket4j.js
@@ -0,0 +1,8 @@
+import BucketConfiguration from './BucketConfiguration.js';
+import Bucket from './Bucket.js';
+
+(() => {
+  // Init here
+})()
+
+Bucket4j
